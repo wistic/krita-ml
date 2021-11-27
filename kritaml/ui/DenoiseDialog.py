13193e735d687dd793dca47dcb5d63f527794bcd
Simpu <id.simpu@gmail.com>
2021-11-28 00:58:42 +0530

    Reduced UI code

--- kritaml/ui/DenoiseDialog.py
+++ kritaml/ui/DenoiseDialog.py
@@ -1,39 +1,48 @@
 from PyQt5.QtWidgets import *
 from .LabelSpinBox import LabelSpinBox
 
+
 class DenoiseDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.setWindowTitle("Denoise")
 
-        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        self.buttonBox = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel
+        )
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
 
-        self.h_input = LabelSpinBox(self)
-        self.h_input.label.setText("h:")
-        self.h_input.spinbox.setValue(10)
-        self.h_input.spinbox.setMinimum(0)
-        self.h_input.spinbox.setMaximum(100)
-        self.h_input.spinbox.setSingleStep(1)
-        self.h_input.spinbox.setDecimals(0)
+        self.h_input = LabelSpinBox(
+            "h:",
+            10,
+            0,
+            100,
+            1,
+            0,
+            self
+        )
 
-        self.t_input = LabelSpinBox(self)
-        self.t_input.label.setText("Template Window Half Size:")
-        self.t_input.spinbox.setValue(3)
-        self.t_input.spinbox.setMinimum(0)
-        self.t_input.spinbox.setMaximum(100)
-        self.t_input.spinbox.setSingleStep(1)
-        self.t_input.spinbox.setDecimals(0)
+        self.t_input = LabelSpinBox(
+            "Template Window Half Size:",
+            3,
+            0,
+            100,
+            1,
+            0,
+            self
+        )
 
-        self.s_input = LabelSpinBox(self)
-        self.s_input.label.setText("Search Window Half Size:")
-        self.s_input.spinbox.setValue(3)
-        self.s_input.spinbox.setMinimum(0)
-        self.s_input.spinbox.setMaximum(100)
-        self.s_input.spinbox.setSingleStep(1)
-        self.s_input.spinbox.setDecimals(0)
+        self.s_input = LabelSpinBox(
+            "Search Window Half Size:",
+            3,
+            0,
+            100,
+            1,
+            0,
+            self
+        )
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.h_input)
@@ -43,14 +52,11 @@ class DenoiseDialog(QDialog):
         self.layout.addWidget(self.buttonBox)
         self.setLayout(self.layout)
 
-    
     def get_h(self):
-        return self.h_input.spinbox.value()
+        return self.h_input.get_value()
 
-    
     def get_template_half_size(self):
-        return self.t_input.spinbox.value()
+        return self.t_input.get_value()
 
-    
     def get_search_half_size(self):
-        return self.s_input.spinbox.value()
+        return self.s_input.get_value()
--- kritaml/ui/LabelSpinBox.py
+++ kritaml/ui/LabelSpinBox.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import *
 
 
 class LabelSpinBox(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, text, value, minimum, maximum, step, decimal, parent=None):
         super().__init__(parent)
         self.label = QLabel(self)
         self.spinbox = QDoubleSpinBox(self)
@@ -10,3 +10,12 @@ class LabelSpinBox(QWidget):
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.spinbox)
         self.setLayout(self.layout)
+        self.label.setText(text)
+        self.spinbox.setValue(value)
+        self.spinbox.setMinimum(minimum)
+        self.spinbox.setMaximum(maximum)
+        self.spinbox.setSingleStep(step)
+        self.spinbox.setDecimals(decimal)
+
+    def get_value(self):
+        return self.spinbox.value()
